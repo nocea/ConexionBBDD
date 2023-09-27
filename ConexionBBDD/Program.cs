9b9834875d84d8efd7299cc14edb7140f7c030c4
Mario Nocea Cabrera <mnoceacabrera@gmail.com>
2023-09-27 14:40:57 +0200

    falta guardar los datos en variables desde el xml

--- ConexionBBDD/Program.cs
+++ ConexionBBDD/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ConexionBBDD
 {
@@ -14,14 +15,21 @@ namespace ConexionBBDD
         {
             int id_libro, edicion;
             string isbn;
-            string titulo, autor;
+            string titulo, autor, server, puerto, usuario, contraseña,bd,conexion;
+            
             NpgsqlConnection conn=null;
             NpgsqlCommand query=null;
             NpgsqlDataReader dr=null;
+            XmlDocument xml=new XmlDocument();
+            XmlNodeList nodo;
+            xml.Load("C:\\Users\\Puesto10\\Desktop\\GITHUB\\ConexionBBDD\\ConexionBBDD\\conexion.xml");
+            Console.WriteLine("Se ha encontrado el archivo");
+            nodo = xml.GetElementsByTagName("conexion");
             
+            conexion = String.Format("Server={0};Port={1}; Username={2};Password={3};Database ={4}", server, puerto, usuario, contraseña, bd);
             try
             {
-                conn = new NpgsqlConnection("Server=localhost;Port=5432; Username=postgres;Password=mariomanu7.;Database = gestorBibliotecaPersonal");
+                conn = new NpgsqlConnection(conexion);
                 Console.WriteLine("Abriendo conexión");
                 conn.Open();
                 query = new NpgsqlCommand("SELECT * FROM gbp_almacen.gbp_alm_cat_libros\r\nORDER BY id_libro ASC ", conn);
